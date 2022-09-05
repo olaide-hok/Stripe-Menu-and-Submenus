fdaae06ed994f2acb73e301907f7575a9ab85c0e
olaide-hok <olaidekareem43@yahoo.com>
2022-09-05 09:45:05 +0100

    Setup Submenu Structure
    Change is Submenu initial state in context.js to false.
    
    Create displySubmenu fn & add to Navbar links as event mouseover

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,6 +6,10 @@ import { useGlobalContext } from './context'
 
 const Navbar = () => {
   const {openSidebar, openSubmenu, closeSubmenu} = useGlobalContext()
+
+  const displaySubmenu = () => {
+    openSubmenu()
+  }
   
   return (
     <nav className="nav">
@@ -19,17 +23,23 @@ const Navbar = () => {
 
         <ul className="nav-links">
           <li>
-            <button className="link-btn">
+            <button className="link-btn"
+              onMouseOver={displaySubmenu}
+            >
               products
             </button>
           </li>
           <li>
-            <button className="link-btn">
+            <button className="link-btn"
+              onMouseOver={displaySubmenu}
+            >
               developer
             </button>
           </li>
           <li>
-            <button className="link-btn">
+            <button className="link-btn"
+              onMouseOver={displaySubmenu}
+            >
               company
             </button>
           </li>
